213b3fede4234cd7d804a78dfac2edaa73a9d3fd
leap2022 <sharawnyambuuurtnasan@gmail.com>
2023-01-12 22:45:40 +0800

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+
+import MainBody from "./sub-components/MainBody";
+
+
+export default function Main() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        position: "relative",
+      }}
+    >
+     
+      <MainBody />
+    
+    </div>
+  );
+}
---
+++ src/components/sub-components/MainBody.jsx
@@ -0,0 +1,5 @@
+export default function MainBody() {
+  return (
+    <div></div>
+  );
+}
